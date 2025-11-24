635808dc33fcaaa7e0239c0897985d543426d5b8
vanshux23 <vanshgrover.work@gmail.com>
2025-11-24 19:03:21 +0530

    fix: add backend entrypoint for vercel

---
+++ backend/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Entrypoint for Vercel
+ * This file is created to satisfy Vercel's search for src/index.ts
+ * even though we are using the api/ directory for serverless functions.
+ */
+import { appRouter } from './api';
+
+export { appRouter };
