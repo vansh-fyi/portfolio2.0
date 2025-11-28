edcc147895a9874271b349514984ff2f0d73ca15
vanshux23 <vanshgrover.work@gmail.com>
2025-11-28 10:23:03 +0530

    Updating AI drop down logo

--- vansh.fyi/src/config/projects.tsx
+++ vansh.fyi/src/config/projects.tsx
@@ -429,15 +429,12 @@ export const projectCategories: ProjectCategory[] = [
   {
     id: 'ai-driven',
     name: 'AI-Driven Projects',
-    icon: (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
-      fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-      stroke-linejoin="round" data-lucide="circle-play"
-      className="lucide lucide-circle-play flex-shrink-0" data-icon-set="lucide"
-      data-icon-replaced="true">
-      <path
-        d="M9 9.003a1 1 0 0 1 1.517-.859l4.997 2.997a1 1 0 0 1 0 1.718l-4.997 2.997A1 1 0 0 1 9 14.996z">
-      </path>
-      <circle cx="12" cy="12" r="10"></circle>
+    icon: (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" className="w-[16px] h-[16px]">
+      <path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z" className=""></path>
+      <path d="M20 3v4"></path>
+      <path d="M22 5h-4"></path>
+      <path d="M4 17v2"></path>
+      <path d="M5 18H3"></path>
     </svg>),
     sections: [
       {
